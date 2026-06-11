75e6f7d761966453cae2b7f5cefd69577e132490
gargavi-oss <gargavi2006@outlook.com>
2026-06-11 15:57:00 +0530

    adding the basic structure of the backend

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {app} from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./.env"
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is running at : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed: ",err);
+})
+
+
+
+
+app.listen()
